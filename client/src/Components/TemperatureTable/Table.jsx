3e4ae346c4497deb2cce4872ce6265993e832a84
Abdur-RahmanIslam2010 <abdelrahmanislam116@gmail.com>
2025-12-05 10:39:09 +0200

    Table adjustments
    Adjusted the TempTable component by div-wrapping the table element inside, and added more adjustments for styling. The new changes in the styling are minor adjustments, along with removing redundant styling code, and making the TempTable scrollable instead of it extending the website down.

--- client/src/Components/TemperatureTable/Table.jsx
+++ client/src/Components/TemperatureTable/Table.jsx
@@ -3,29 +3,31 @@ import './Table.css';
 function TempTable(props) {
     return (
     <div className='temp-table'>
-        <div>
+        <div className='table-date-heading'>
             <button id={'left-arrow'} onClick={props.leftClick}></button>
             <h2>{props.date}</h2>
             <button id={'right-arrow'} onClick={props.rightClick}></button>
         </div>
-        <table>
-            <thead>
-                <tr>
-                    <th>Time</th>
-                    <th>Temperature</th>
-                    <th>Humidity</th>
-                </tr>
-            </thead>
-            <tbody>
-                {props.time.map((_, i) => {return (
-                    <tr key={i}>
-                        <td>{props.time[i]}</td>
-                        <td>{props.temp[i]}</td>
-                        <td>{props.hum[i]}</td>
+        <div className='data-table'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Time</th>
+                        <th>Temperature</th>
+                        <th>Humidity</th>
                     </tr>
-                );})}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {props.time.map((_, i) => {return (
+                        <tr key={i}>
+                            <td>{props.time[i]}</td>
+                            <td>{props.temp[i]}</td>
+                            <td>{props.hum[i]}</td>
+                        </tr>
+                    );})}
+                </tbody>
+            </table>
+        </div>
     </div>
     );
 }
