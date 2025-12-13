22d495bc6fd03325a996c7c8ae0c1150fae3ed70
Abdur-RahmanIslam2010 <abdelrahmanislam116@gmail.com>
2025-12-13 15:58:50 +0200

    Delete vercel.json and remove Vercel configurations

--- server/server.js
+++ server/server.js
@@ -33,13 +33,10 @@ app.get('/icons/:icon', (req, res) => {
     };
     res.sendFile(icons[icon]);
 });
-if (!process.env.VERCEL) {
-    app.listen(port, (err) => {
-        if (err) throw err;
-        else {
-            console.log(`Server started and is listening at http://localhost:${port}`);
-        }
-    });
-}
 
-module.exports = app;
+app.listen(port, (err) => {
+    if (err) throw err;
+    else {
+        console.log(`Server started and is listening at http://localhost:${port}`);
+    }
+});
